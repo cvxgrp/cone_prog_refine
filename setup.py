b469d0f8d3e1c619d7d20d6df0bfcce2ffaa1f57
Enzo Busseti <enzo.busseti@me.com>
2019-03-07 14:07:57 -0800

    version bumb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cpsr',
-    version='0.0.2',
+    version='0.0.3',
     description='Cone Problem Solution Refinement.',
     author='Enzo Busseti',
     packages=['cpsr'],
